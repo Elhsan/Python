231fc1bf02a5bb93b7b9c9596a71600b27794cfa
Elshan <elshanaliev2009@gmail.com>
2023-06-02 15:33:34 +0500

    Elshan2

--- main2.py
+++ main2.py
@@ -23,8 +23,8 @@ while vopros_Polzovatelya not in [1, 2]:
         if reg_parol == reg_parol2:
             print("Вы успешно зарегестрировались")
             vopros_Polzovatelya2 = None
-            while vopros_Polzovatelya2 not in [1, 2]:
-                vopros_Polzovatelya2 = int(input("Выберите опцию (1 - игра угадай число, 2 - настройки): ")) 
+            while vopros_Polzovatelya2 not in [1, 2, 3]:
+                vopros_Polzovatelya2 = int(input("Выберите опцию (1 - игра угадай число, 2 - камень ножница или бумага, 3 - настройки): ")) 
                 if vopros_Polzovatelya2 == 1:
                     print("Вы вошли в игру угадай число")
                     print("Добро пожаловать в игру угадай число для Python!")
@@ -50,9 +50,32 @@ while vopros_Polzovatelya not in [1, 2]:
                         else:
                             print("Правильно! Вы угадали число за", tries, "попыток.")
                 elif vopros_Polzovatelya2 == 2:
+                   print("Добро пожаловать в игру 'Камень-Ножницы-Бумага'!\n")
+                   while True:
+                       print("Выберите: камень (к), ножницы (н) или бумагу (б): ")
+                       player_choice = input().lower()
+                   
+                       if player_choice not in ('к', 'н', 'б'):
+                           print("Вы выбрали неверный вариант, попробуйте еще раз!")
+                           continue
+                       
+                       computer_choice = random.choice(['к', 'н', 'б'])
+                       print("Компьютер выбрал", computer_choice)
+                   
+                       if player_choice == computer_choice:
+                           print("Ничья!")
+                       elif (player_choice == 'к' and computer_choice == 'н') or (player_choice == 'н' and computer_choice == 'б') or (player_choice == 'б' and computer_choice == 'к'):
+                           print("Вы выиграли!")
+                       else:
+                           print("Вы проиграли!")
+                   
+                       play_again = input("Хотите сыграть еще раз? (да/нет): ")
+                       if play_again.lower() != 'да':
+                           break
+                elif vopros_Polzovatelya2 == 3:
                     print("Вы вошли в Настройки здесь пока ничего нет")
+                
 
-    
         elif reg_parol != reg_parol2:
             print("Ваши пароль не савподают!!!")
     
@@ -60,19 +83,18 @@ while vopros_Polzovatelya not in [1, 2]:
     
         login = "Elshanaliev2009"
         password = 123456789
-    
+
         login_Alisher_aka = "AlisherXujanov"
         password_Alisher_aka = 12345
-    
     
         login_proverka = input("Введите адрес электронной почты: ")
         parol_proverka = input("Введите проль: ")
     
         if  login == login_proverka and password == int(parol_proverka) or login_proverka == login_Alisher_aka and password_Alisher_aka == int(parol_proverka):
             print("вы вошли в акаунт!")
-            
+
             vopros_Polzovatelya2 = None
-            while vopros_Polzovatelya2 not in [1, 2]:
+            while vopros_Polzovatelya2 not in [1, 2, 3]:
                 vopros_Polzovatelya2 = int(input("Выберите опцию (1 - игра угадай число, 2 - настройки): "))
 
                 if vopros_Polzovatelya2 == 1:
@@ -100,9 +122,32 @@ while vopros_Polzovatelya not in [1, 2]:
                         else:
                             print("Правильно! Вы угадали число за", tries, "попыток.")
                 elif vopros_Polzovatelya2 == 2:
+                   print("Добро пожаловать в игру 'Камень-Ножницы-Бумага'!\n")
+                   while True:
+                       print("Выберите: камень (к), ножницы (н) или бумагу (б): ")
+                       player_choice = input().lower()
+                   
+                       if player_choice not in ('к', 'н', 'б'):
+                           print("Вы выбрали неверный вариант, попробуйте еще раз!")
+                           continue
+                       
+                       computer_choice = random.choice(['к', 'н', 'б'])
+                       print("Компьютер выбрал", computer_choice)
+                   
+                       if player_choice == computer_choice:
+                           print("Ничья!")
+                       elif (player_choice == 'к' and computer_choice == 'н') or (player_choice == 'н' and computer_choice == 'б') or (player_choice == 'б' and computer_choice == 'к'):
+                           print("Вы выиграли!")
+                       else:
+                           print("Вы проиграли!")
+                   
+                       play_again = input("Хотите сыграть еще раз? (да/нет): ")
+                       if play_again.lower() != 'да':
+                           break
+                elif vopros_Polzovatelya2 == 3:
                     print("Вы вошли в Настройки здесь пока ничего нет")
-
-            
+                
+ 
         elif login_proverka != login and int(parol_proverka) != password:
             print("Логин или пароль неверны")
     
